f1dc77299bd9533acce5c6ee2529a3f1513bbc13
Amin_shdl <135274717+iaamin@users.noreply.github.com>
2023-07-02 19:51:01 +0330

    index,js added.

---
+++ index.js
@@ -0,0 +1,34 @@
+//import App from './App';
+import Product from './components/product';
+//import ReactDom form 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const element = <h1>Hello My Friend</h1>
+
+ReactDom.render(<App/>, document.getElementById(root));
+//----------------------------------------
+
+//import { walk, talk } from "./person";
+//export default :
+// import walk, {talk} from "./person";
+
+
+
+// walk()
+// talk()
+//----------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
